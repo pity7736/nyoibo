6db025483650f99ce896b4ab7e5938e53b4ff076
Julián Cortés <pity7736@gmail.com>
2020-07-12 22:09:13 -0500

    chore: no required cython to install nyoibo

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from Cython.Build import cythonize
 from setuptools import setup, Extension
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+except ImportError:
+    USE_CYTHON = False
 
+if USE_CYTHON:
+    ext = 'pyx'
+else:
+    ext = 'c'
 
 extensions = (
-    Extension('nyoibo.fields', ['nyoibo/fields.pyx']),
-    Extension('nyoibo.entities.entity', ['nyoibo/entities/entity.pyx'])
+    Extension('nyoibo.fields', [f'nyoibo/fields.{ext}']),
+    Extension('nyoibo.entities.entity', [f'nyoibo/entities/entity.{ext}'])
 )
-setup(
-    name='nyoibo',
-    ext_modules=cythonize(
+
+if USE_CYTHON:
+    extensions = cythonize(
         extensions,
         compiler_directives={'language_level': 3}
     )
+
+setup(
+    name='nyoibo',
+    ext_modules=extensions
 )
