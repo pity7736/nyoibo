41b6b6c78f5f5a56a84174f307b3c0f20269a185
Julián Cortés <julian.cortes77@pm.me>
2023-05-15 20:28:57 -0500

    deps: update dev dependencies

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ with open('README.rst') as f:
     readme = f.read()
 
 tests_require = [
-    'pytest==6.2.5',
-    'pytest-cov==3.0.0'
+    'pytest~=7.0.0',
+    'pytest-cov~=3.0.0'
 ]
 
 extensions = [
